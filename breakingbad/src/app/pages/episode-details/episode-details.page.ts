9db939f5ad82ede75e765d5c2a4232dc2a9adeba
dianaboo <ing.diana.gtz@gmail.com>
2020-04-19 10:46:05 +0000

    breakingbad

--- breakingbad/src/app/pages/episode-details/episode-details.page.ts
+++ breakingbad/src/app/pages/episode-details/episode-details.page.ts
@@ -1,3 +1,4 @@
+import { FavouriteService } from './../../services/favourite.service';
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
@@ -14,18 +15,38 @@ export class EpisodeDetailsPage implements OnInit {
 
     episode: any;
     episodeId = null;
+    isFavourite = false;
 
     constructor(private activatedRoute: ActivatedRoute,
-     private api: ApiService) { }
+        private api: ApiService, private favouriteService: FavouriteService) { }
 
 
-   ngOnInit() {
+    ngOnInit() {
         this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
+
         this.api.getEpisode(this.episodeId).subscribe(res => {
             this.episode = res[0];
             console.log(JSON.stringify(this.episode.episode_id));
         });
+
+        
+
+
+        this.favouriteService.isFavourite(this.episodeId).then(isFav => {
+            this.isFavourite = isFav;
+        });
+    }
+
+    favouriteEpisode() {
+        this.favouriteService.favouriteEpisode(this.episodeId).then(() => {
+            this.isFavourite = true;
+        });
+    }
+
+    unfavouriteEpisode() {
+        this.favouriteService.unfavouriteEpisode(this.episodeId).then(() => {
+            this.isFavourite = false;
+        });
     }
 }
 
-   
